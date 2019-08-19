0b769b46a8cab51990ef491068df08b1966fb6d9
duht <duht@aliyun.com>
2019-08-19 08:20:34 +0800

    add ex01.cpp

---
+++ ex01.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <functional>
+
+typedef std::function<void()> AAA_func;
+
+class AAA
+{
+public:
+    void show ()
+    {
+        printf ("AAA show\n");
+    }
+
+    AAA_func func = show;
+};
+
+int
+main ()
+{
+    AAA aaa;
+    /// AAA_func func = std::bind(&AAA::show, aaa);
+
+    aaa.func ();
+}
